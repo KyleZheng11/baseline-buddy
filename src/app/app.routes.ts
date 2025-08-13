da750d7813e6a43abf039956e5ecaba90d4da0ed
KyleZheng11 <108999604+KyleZheng11@users.noreply.github.com>
2025-08-13 16:53:01 -0400

    feat: added login form/button, still working on connectin database to store usernames

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page';
+import { Login } from './login/login';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomePage
+    },
+    {
+        path: 'login',
+        component: Login
     }
 ];
--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { Button } from 'primeng/button';
+import { Login } from "../login/login";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ButtonModule, Button],
+  imports: [ButtonModule, Login],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
